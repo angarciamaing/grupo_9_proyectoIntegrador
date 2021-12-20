238be302433283eda87da8341db5cc20841772c0
adrisanc <yasminadrianasanchez@gmail.com>
2021-12-20 00:34:45 -0500

    [ADD] Archivos html y css pagina detalle del producto, con media queries min-width 768px

--- app.js
+++ app.js
@@ -11,3 +11,4 @@ app.use(express.static(publicPath));
 app.listen(4000, () => console.log("Servidor corriendo en el puerto: 4000"));
 
 app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html')));
+app.get("/detalle-producto", (req, res) => res.sendFile(path.resolve(__dirname, './views/detalle-producto.html')));
