b35215c6c5b68efff8f6c15fcdcd686be664731d
adrisanc <yasminadrianasanchez@gmail.com>
2022-02-22 21:38:38 -0500

    [ADD] controlador modificaciones

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,16 +1,27 @@
-
+const fs = require('fs');
 const path = require('path');
 
-
+const productsFilePath = path.join(__dirname,'../database/products.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath,'utf-8'));
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+const nuevoLanzamiento = products.filter((producto)=> producto.lanzamiento ==='nuevo');
+const peliculas = products.filter((producto) => producto.category ==="peliculas" );
 
 module.exports = {
     
     home: (red, res) => {
-        res.render('home');
+        res.render('home',{
+            toThousand,
+            nuevoLanzamiento
+    
+        });
     },
 
     detalleProducto: (req, res) => {
-        res.render('detalle-producto');
+        res.render('detalle-producto',{
+            toThousand,
+            peliculas
+        });
     },
 
     shoppingCart: (req, res) => {
