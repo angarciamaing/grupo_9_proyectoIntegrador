d2f0543f52aa1a2ce0f1ca400f6d2c0c9f7be00e
EstebanNavas <arcoesteb@live.com>
2022-04-11 16:40:21 +0200

    Se crea archivo de validaciones Front

--- public/js/productsCreateValidation.js
+++ public/js/productsCreateValidation.js
@@ -1,11 +1,29 @@
 window.onload = () => {
-    const name = document.getElementById('name');  // Coloca el  cursor en el pirmer campo del formulario al recargar la página
-    name.focus();
+    const name = document.getElementById('name'); 
+    const description = document.getElementById('description');
+    const image = document.getElementById('image');
+    
+    name.focus(); // Coloca el  cursor en el pirmer campo del formulario al recargar la página
 
-    // const form = document.getElementById('editProductForm');
-    // form.onsubmit = (e) => {
-    //     e.preventDefault();
-    //     console.log('hola desde sunmit');
+    console.log('hola');
 
-    // }
+    const form = document.getElementById('editProductForm');
+    form.onsubmit = (e) => {
+       
+        if(name.value === ""){
+            e.preventDefault();
+            // alert('Nombre del producto no puede estar vacio')
+            name.classList.add('is-invalid')
+        }
+
+        if(description.value === ""){
+            e.preventDefault();
+            alert('Descripción del producto no puede estar vacio')
+        }
+
+        if(image.value === ""){
+            e.preventDefault();
+            alert('Debes seleccionar una imagen')
+        }
+    }
 }
