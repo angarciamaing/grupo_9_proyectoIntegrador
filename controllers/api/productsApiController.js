ff4efab524ab053836720e5def6811e33e04a3a5
angarciama <angarciama@unal.edu.co>
2022-04-28 16:35:19 -0500

    productsApiController

--- controllers/api/productsApiController.js
+++ controllers/api/productsApiController.js
@@ -7,33 +7,40 @@ const CategoryProduct = db.CategoryProduct
 const productsApiController = {
 
     'products': async (req, res) => {
-        const products = await db.Product.findAll();
-        const response = {
-            count: products.length,
-            //countByCategory → objeto literal con una propiedad por categoría con el total de productos.
-            products: {
-                products: products,
-                //un array con principal relación de uno a muchos (ej:categories).
-                //detail → URL para obtener el detalle.
-                url: '/api/products/'
-            } 
-
-        }
-        res.json(response);
-    },
-    'detail': (req, res) => {
-        db.Product.findByPk(req.params.id)
-        .then(product => {
-            const response = {
-                //array categorias
-                //Una URL para la imagen del producto (para mostrar la imagen).
-                product: product,
-                url: '/api/products/:id'
-                    
+        try {
+            const products = await db.Product.findAll();
+            const categoryProduct = await db.CategoryProduct.findAll();
+            const data = {
+                count: products.length,
+                //countByCategory → objeto literal con una propiedad por categoría con el total de productos.
+                countByCategory: categoryProduct,
+                products: products.map(product => {
+                return{
+                    products: product,
+                    //un array con principal relación de uno a muchos (ej:categories).
+                    category: product.name_category,
+                    url: 'http://localhost:4000/api/products/' + product.id
+                }
+                })
             }
-            res.json(response);
-        });
+            res.json(data);
+        }catch(error){
+            res.json('Algo salió mal')
+        }
+    },
 
+    'detail': async (req, res) => {
+        try {
+            const product = await db.Product.findByPk(req.params.id);
+            const data = {
+                product:product,
+                //un array por cada relación de uno a muchos (categories, colors,sizes, etc)
+                url: 'http://localhost:4000/img/productsImages/' + product.image,
+            }
+            res.json(data);
+        }catch(error){
+            res.json('Algo salió mal')
+        }
     }
 
 }
