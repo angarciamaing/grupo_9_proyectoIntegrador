37d64303ceb89238119036cb401a580167c22930
angarciama <angarciama@unal.edu.co>
2022-05-31 10:16:48 -0500

    productCreateValidationFront

--- server/public/js/productsCreateValidation.js
+++ server/public/js/productsCreateValidation.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    const erroresList = document.getElementById('erroresList'); 
+    
     const name = document.getElementById('name'); 
     const description = document.getElementById('description');
     const image = document.getElementById('image');
@@ -7,50 +7,75 @@ window.onload = () => {
     
     name.focus(); // Coloca el  cursor en el pirmer campo del formulario al recargar la página
 
-    console.log('hola');
-
     const form = document.getElementById('createProductForm');
     form.onsubmit = (e) => {
 
         const errores = [];
        
-        if(name.value === "" && name.value <= 4){     
+        //Nombre del producto
+        if(name.value == ""){     
             name.classList.add('is-invalid');                      // Agrega borde rojo si em campo está vacio 
-            errores.push('Debes de elegir un nombre');
+            errores.push('error name');
+            errores.name = 'Debe ingresar el nombre del producto'
+            nameFront.innerText = errores.name
+        } else if(name.value.length < 5) {
+            name.classList.add('is-invalid');                      // Agrega borde rojo si em campo está vacio 
+            errores.push('error name');
+            errores.name = 'El nombre debe tener mínimo 5 caracteres'
+            nameFront.innerText = errores.name
+        } else {            
+            delete errores.name
+            nameFront.innerText = ""
         }
 
-        if(description.value === ""){
+        //Descripcion del producto
+        if(description.value == ""){
             description.classList.add('is-invalid');
-            errores.push('Debes de agregar una descripción');
-            
+            errores.push('error description');
+            errores.description = 'Debe ingresar la descripción del producto'
+            descriptionFront.innerText = errores.description
+        } else if(description.value.length < 20) {
+            description.classList.add('is-invalid');
+            errores.push('error description');
+            errores.description = 'La descripción debe tener mínimo 20 caracteres'
+            descriptionFront.innerText = errores.description
+        } else {        
+            delete errores.description
+            descriptionFront.innerText = ""
         }
 
-        if(image.value === ""){
-            errores.push('Debes de elegir una imagen');
-           
+        //Imagen del producto
+        if(image.value == ""){
+            description.classList.add('is-invalid');
+            errores.push('error image');
+            errores.image = 'Debe seleccionar una imagen'
+            imageFront.innerText = errores.image
+        } else {
+            delete errores.image
+            imageFront.innerText = ""
         }
 
-        if(price.value === ""){
+        //Precio del producto
+        if(price.value == ""){
             price.classList.add('is-invalid');
-            errores.push('Debes de asignar un precio');
-            
+            errores.push('error price');
+            errores.price = 'Debe ingresar el valor del producto'
+            priceFront.innerText = errores.price
+        } else {
+            delete errores.price
+            priceFront.innerText = ""
         }
 
         if(errores.length > 0){
-            e.preventDefault();
-            for (const error of errores) {
-                erroresList.innerHTML += "<li>" + error + "</li>"
-            }
+            e.preventDefault();            
         }
     }
 
     const removeIsInvalid = (e) => {
         e.target.classList.remove('is-invalid');            // Quita el borde rojo cuando el cliente escribe en el campo 
     }
-
     name.onkeydown = removeIsInvalid
     description.onkeydown = removeIsInvalid
-    price.onkeydown = removeIsInvalid
-    
+    price.onkeydown = removeIsInvalid    
 
 }
